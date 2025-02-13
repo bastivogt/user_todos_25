e8f8c31fa179518c6b6db9a2144db440f2e0f520
Sevo <sevodo.dessau@gmail.com>
2025-02-13 02:05:56 +0100

    add back-link-meta template

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", (e) => {
     console.log("Hello, world by app.js!");
 });
+
+
+const backLinks = document.querySelectorAll(".back-link");
+
+
+
+backLinks.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("back-link clicked");
+        history.back();
+    });
+});
